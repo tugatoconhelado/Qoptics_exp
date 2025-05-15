89186c1699dc8ac81406b483f9a8a0f95d2027ab
Nicolás Vera Castillo <nicolas.vera@uc.cl>
2025-05-15 10:59:42 -0400

    Added newest version of Ivan's code

--- src/qudi/gui/pulsed_esr/pulsed_esr_gui.py
+++ src/qudi/gui/pulsed_esr/pulsed_esr_gui.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['TimeTraceGui']
+__all__ = ["TimeTraceGui"]
 
 import os
 from PySide2 import QtCore
 from PySide2.QtWidgets import QDialog, QWidget, QFileDialog, QMessageBox
-from PySide2.QtCore import Slot, Qt
+from PySide2.QtCore import Slot, Qt, Signal
 
 from qudi.core.module import GuiBase
 from qudi.core.connector import Connector
@@ -19,9 +19,18 @@ import pyqtgraph as pg
 
 
 class PulsedESRGui(GuiBase):
-    """ This is a simple template GUI measurement module for qudi """
-    
-    _pulsed_esr_logic = Connector(name='pulsed_esr_logic', interface='PulsedESRLogic')
+    """This is a simple template GUI measurement module for qudi"""
+
+    add_channel_to_logic_signal = Signal(int, list, str, int)
+    prepare_frame_signal = Signal(int)
+    add_pulse_to_logic_signal = Signal(float, float, str, str, list, int)
+    run_exp_to_logic_signal = Signal(int, int)
+    stop_exp_to_logic_signal = Signal()
+    frame_to_logic_signal = Signal(int)
+    simulation_to_logic = Signal(int, int, int)
+    clear_logic_to_signal = Signal()
+
+    _pulsed_esr_logic = Connector(name="pulsed_esr_logic", interface="PulsedESRLogic")
 
     # Declare static parameters that can/must be declared in the qudi configuration
     # _my_config_option = ConfigOption(name='my_config_option', default=1, missing='warn')
@@ -32,41 +41,66 @@ class PulsedESRGui(GuiBase):
 
     def on_activate(self) -> None:
 
-        #print("Initializing UI...")
-        self._mw = PulsedESRMainWindow() # initializes the UI form
-
-        self._mw.run_sequence_button.clicked.connect(self.Run_Experiment_Gui)
-        self._mw.stop_sequence_button.clicked.connect(self.Stop_Experiment_Gui)
+        self._mw = PulsedESRMainWindow()  # initializes the UI form
 
         ########## SIGNALS and connectios ##########
-        
-          ##### ADDING CHANNELS #####
+
+        ##### ADDING CHANNELS #####
+        # from gui window to gui slots
         self._mw.add_channel_button.clicked.connect(self.add_channel_gui)
         self._pulsed_esr_logic().adding_flag_to_list.connect(self.update_list_channels)
-         
+        # from gui slots to logic
+        self.add_channel_to_logic_signal.connect(self._pulsed_esr_logic().add_channel)
+
         ######## Adding and varying pulses ##############
+        # from gui window to gui slots
         self._pulsed_esr_logic().error_str_signal.connect(self.show_error_message)
         self._mw.add_pulse_button.clicked.connect(self.add_pulse_gui)
         self._mw.iteration_start_spinbox.valueChanged.connect(self.set_max)
+        # from gui slots to logic
+        self.add_pulse_to_logic_signal.connect(
+            self._pulsed_esr_logic().add_pulse_to_channel
+        )
 
         ######## Selecting Frame for Display #######
-        self._mw.iteration_frame_spinbox.valueChanged.connect(self.Prepare_Frame)
-        self._mw.update_button.clicked.connect(self.Prepare_Frame)
-        self._pulsed_esr_logic().add_frame_to_graph.connect(self.Show_Frame)
+        # from gui window to gui slots
+        self._mw.iteration_frame_spinbox.setMinimum(1)
+        self._mw.iteration_frame_spinbox.valueChanged.connect(self.prepare_frame)
+        self._mw.update_button.clicked.connect(self.prepare_frame)
+        # from gui slots to logic
+        self.frame_to_logic_signal.connect(self._pulsed_esr_logic().prepare_frame)
+        self._pulsed_esr_logic().frame_data_signal.connect(
+            self._mw.create_frame
+        ) 
 
         ####### Run Simulation ########
-        self._mw.stop_simulation_button.clicked.connect(self.Start_Simulation)
-        self._pulsed_esr_logic().next_frame_signal.connect(self.Prepare_next_Frame_Simulation)
+        # from gui window to gui slots
+        self._mw.stop_simulation_button.clicked.connect(self.start_simulation)
+        self._pulsed_esr_logic().next_frame_signal.connect(
+            self.prepare_next_frame_simulation
+        )
         self._pulsed_esr_logic().add_iteration_txt.connect(self.add_iteration_text)
+        # from gui slots to logic
+        self.frame_to_logic_signal.connect(self._pulsed_esr_logic().prepare_frame)
+        self.simulation_to_logic.connect(self._pulsed_esr_logic().Run_Simulation)
+
+        ####### RUn Experiment #######
+        # from gui window to gui slots
+        self._mw.run_sequence_button.clicked.connect(self.run_experiment_gui)
+        self._mw.stop_sequence_button.clicked.connect(self.stop_experiment_gui)
+        # from gui slots to logic
+        self.run_exp_to_logic_signal.connect(self._pulsed_esr_logic().Run_experiment)
+        self.stop_exp_to_logic_signal.connect(self._pulsed_esr_logic().Stop_Experiment)
+
         ###### Clear Gui #######
-        self._mw.clear_channels_button.clicked.connect(self.Clear_Gui())
-        ##### ADDING CHANNELS #####
+        # from gui window to gui slots
+        self._mw.clear_channels_button.clicked.connect(self.clear_gui)
 
         self.show()
 
     def on_deactivate(self) -> None:
         # Disconnect all connections done in "on_activate"
-        #self._template_logic().sigCounterUpdated.disconnect(self._mw.count_spinbox.setValue)
+        # self._template_logic().sigCounterupdate_buttond.disconnect(self._mw.count_spinbox.setValue)
         # Use "plain" disconnects (without argument) only on signals owned by this module
         # Close main window
         self._mw.close()
@@ -76,90 +110,116 @@ class PulsedESRGui(GuiBase):
         This function is called when the user clicks the "Add Channel" button.
         It checks if the channel is valid and adds it to the list.
         """
-        channel_tag = self._mw.Channel_Identifier.currentIndex()
+        channel_tag = self._mw.channel_identifier_combobox.currentIndex()
         print(f"channel added:{channel_tag}")
-        delay= [self._mw.Delay_ON.value(),self._mw.Delay_OFF.value()]
-        channel_label = self._mw.Type_Channel.text()#we get the label of the channel from the gui
-        channel_label=channel_label.lower() #we leave it undercase
-        channel_count=self._mw.Channel_Identifier.count()
-        self._pulsed_esr_logic().add_channel(channel_tag,delay,channel_label,channel_count)
+        delay = [self._mw.delay_on_spinbox.value(), self._mw.delay_off_spinbox.value()]
+        channel_label = (
+            self._mw.channel_type_line_edit.text()
+        )  # we get the label of the channel from the gui
+        channel_label = channel_label.lower()  # we leave it undercase
+        channel_count = self._mw.channel_identifier_combobox.count()
+        # self._pulsed_esr_logic().add_channel(channel_tag,delay,channel_label,channel_count)
+        self.add_channel_to_logic_signal.emit(
+            channel_tag, delay, channel_label, channel_count
+        )
 
-
-
-    @Slot(str) #The @Slot decorator in PySide2 is used to explicitly define a method as a slot, which can be connected to a signal. It improves performance and type safety by specifying the expected argument types.
+    @Slot(str)
     def update_list_channels(self, flag_str):
         """
         This function is called when a channel is added to the list.
         It updates the list of channels in the GUI.
         """
-        #print(f"Adding channel: {flag_str}")
+        # print(f"Adding channel: {flag_str}")
         self._mw.channel_list_listwidget.addItem(flag_str)
 
-
-    ##### ADDING PULSES ######
     def add_pulse_gui(self):
         """
         This function is called when the user clicks the "Add Pulse" button.
         It checks if the pulse is valid and adds it to the list.
         """
-        start_time = self._mw.StartTime.value()
-        width = self._mw.Puls_Width.value()
-        channel_tag = self._mw.Channel_Pulse.currentIndex() #we get the channel from the gui
-        function_width=self._mw.Function_Width.text() #we get the function from the gui
-        function_start=self._mw.Function_Start.text()
-        iteration_range = [self._mw.Iterations_start.value(),self._mw.Iterations_end.value()]
-        self._pulsed_esr_logic().add_pulse_to_channel(start_time, width,function_width,function_start,iteration_range, channel_tag)
+        start_time = self._mw.start_time_spinbox.value()
+        width = self._mw.pulse_width_spinbox.value()
+        channel_tag = (
+            self._mw.pulse_channel_combobox.currentIndex()
+        )  # we get the channel from the gui
+        function_width = (
+            self._mw.width_function_line_edit.text()
+        )  # we get the function from the gui
+        function_start = self._mw.start_function_line_edit.text()
+        iteration_range = [
+            self._mw.iteration_start_spinbox.value(),
+            self._mw.iteration_end_spinbox.value(),
+        ]
+        # self._pulsed_esr_logic().add_pulse_to_channel(start_time, width,function_width,function_start,iteration_range, channel_tag)
+        self.add_pulse_to_logic_signal.emit(
+            start_time,
+            width,
+            function_width,
+            function_start,
+            iteration_range,
+            channel_tag,
+        )
 
-    def set_max(self): # as soon as I change the value fo the Iteration_start, the Iteration _end, allow numbers bigger than the Iterations_start
-        self._mw.Iterations_end.setMinimum(self._mw.Iterations_start.value()+1) 
+    def set_max(self):
+        self._mw.iteration_end_spinbox.setMinimum(
+            self._mw.iteration_start_spinbox.value() + 1
+        )
 
+    def run_experiment_gui(self):
+        value_loop = self._mw.loop_sequence_spinbox.value()
+        Type = self._mw.type_variation_combobox.currentIndex()
+        # self._pulsed_esr_logic().Run_experiment(value_loop,Type)
+        self.run_exp_to_logic_signal.emit(value_loop, Type)
 
+    def stop_experiment_gui(self):
+        # self._pulsed_esr_logic().Stop_Experiment()
+        self.stop_exp_to_logic_signal.emit()
 
-    #### RUN Experiment ####
-    def Run_Experiment_Gui(self):
-        value_loop=self._mw.Loop_Sequence.value()
-        self._pulsed_esr_logic().Run_experiment(value_loop)
-    def Stop_Experiment_Gui(self):
-        self._pulsed_esr_logic().Stop_Experiment()
-
-     ######## Selecting Frame for Display #######
-    def Prepare_Frame(self):
-        Frame_i=self._mw.Iteration_frame.value()
+    def prepare_frame(self):
+        Frame_i = self._mw.iteration_frame_spinbox.value()
         self._mw.sequence_diagram_plot.clear()
-        self._mw.sequence_diagram_plot.enableAutoRange(axis=pg.ViewBox.XAxis, enable=False)
-        self._mw.sequence_diagram_plot.setXRange(0, self._pulsed_esr_logic().Max_end_time, padding=0)  # or whatever fixed length you want
-        self._pulsed_esr_logic().Prepare_Frame(Frame_i) #this prepares the 
-        #we set the value of the x axis to the largest end time of all the iterations from all the channel
+        self._mw.sequence_diagram_plot.enableAutoRange(
+            axis=pg.ViewBox.XAxis, enable=False
+        )
+        self._mw.sequence_diagram_plot.setXRange(
+            0, self._pulsed_esr_logic().Max_end_time, padding=0
+        )  # or whatever fixed length you want
+        self.frame_to_logic_signal.emit(Frame_i)
 
-    def Show_Frame(self,sequence):
-        self._mw.sequence_diagram_plot.addItem(sequence)
 
-    ####### Simulation #######
-    def Start_Simulation(self):
-        initial_frame=self._mw.Iteration_frame.value()
+    def start_simulation(self):
+        initial_frame = self._mw.iteration_frame_spinbox.value()
         print(f"initial frame:{initial_frame}")
-        ms=self._mw.ms.value()
+        ms = self._mw.ms.value()
         print(f"ms:{ms}")
-        value_loop=self._mw.Loop_Sequence.value()
+        value_loop = self._mw.loop_sequence_spinbox.value()
         print(f"value_loop: {value_loop}")
-        self._pulsed_esr_logic().Run_Simulation(initial_frame,value_loop,ms)
+        # self._pulsed_esr_logic().Run_Simulation(initial_frame,value_loop,ms)
+        self.simulation_to_logic.emit(initial_frame, value_loop, ms)
         # Disable the button after click
-    def Prepare_next_Frame_Simulation(self,Frame_i):
+
+    def prepare_next_frame_simulation(self, Frame_i):
         self._mw.sequence_diagram_plot.clear()
-        self._mw.sequence_diagram_plot.enableAutoRange(axis=pg.ViewBox.XAxis, enable=False)
-        self._mw.sequence_diagram_plot.setXRange(0, self._pulsed_esr_logic().Max_end_time, padding=0)  # or whatever fixed length you want
-        self._pulsed_esr_logic().Prepare_Frame(Frame_i) #this prepares the 
-    def add_iteration_text(self,text):
-        self._mw.current_iteration_label.setText(text)
-    
-    ###### CLearing Gui ######
-    def Clear_Gui(self):
+        self._mw.sequence_diagram_plot.enableAutoRange(
+            axis=pg.ViewBox.XAxis, enable=False
+        )
+        self._mw.sequence_diagram_plot.setXRange(
+            0, self._pulsed_esr_logic().Max_end_time, padding=0
+        )  # or whatever fixed length you want
+        # self._pulsed_esr_logic().prepare_frame(Frame_i) #this prepares the
+        self.frame_to_logic_signal.emit(Frame_i)
+
+    def add_iteration_text(self, text):
+        self._mw.current_iteration.setText(text)
+
+    def clear_gui(self):
         self._mw.channel_list_listwidget.clear()
         self._mw.sequence_diagram_plot.clear()
-        self._mw.loop_duration_label.setText("Duration: ( )")
-        self._mw.current_iteration_label.setText("current iteration: ( )")
-        self._pulsed_esr_logic().Clearing_Gui()
-        
+        self._mw.Duration_Loop.setText("Duration: ( )")
+        self._mw.current_iteration.setText("current iteration: ( )")
+        # self._pulsed_esr_logic().Clearing_Gui()
+        self.clear_logic_to_signal.emit()
+
     @Slot(str)
     def show_error_message(self, error_str):
         """
@@ -172,8 +232,7 @@ class PulsedESRGui(GuiBase):
         dlg.setStandardButtons(QMessageBox.Ok)
         dlg.exec_()
 
-
     def show(self) -> None:
-        """ Show the main window and raise it above all others """
+        """Show the main window and raise it above all others"""
         self._mw.show()
         self._mw.raise_()
--- src/qudi/gui/pulsed_esr/pulsed_esr_mainwindow.py
+++ src/qudi/gui/pulsed_esr/pulsed_esr_mainwindow.py
@@ -1,5 +1,5 @@
 from PySide2.QtWidgets import QDialog, QWidget, QMainWindow, QApplication
-from PySide2.QtCore import Slot, Signal, QDir
+from PySide2.QtCore import Slot, Signal, QDir, QObject
 from PySide2.QtGui import QFont
 from qudi.util.uic import loadUi
 import numpy as np
@@ -14,14 +14,150 @@ class PulsedESRMainWindow(QMainWindow):
     """
 
     start_experiment_signal = Signal(int, float, float)
-    
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        loadUi(
-            os.path.join(os.path.dirname(__file__), 'pulsed_esr2.ui'),
-            self
+        loadUi(os.path.join(os.path.dirname(__file__), "pulsed_esr2.ui"), self)
+
+        self.iteration_start_spinbox.valueChanged.connect(self._set_max_iteration_end)
+
+    def _set_max_iteration_end(self):
+        """
+        This function is called when the user changes the start time of the iteration.
+        It sets the maximum end time of the iteration.
+        """
+
+        self._mw.iteration_end_spinbox.setMinimum(
+            self._mw.iteration_start_spinbox.value() + 1
         )
+
+    def create_frame(self, tags_colors, sequences_all_channels, frame_i, max_end_time):
+
+        frame = Frame(tags_colors, sequences_all_channels, frame_i, max_end_time)
+        frame.display_frame()  # here we send the value to build the Frame
+        sequence_for_graph = frame.plot_sequences
+        for sequence_frame in sequence_for_graph:
+            self.sequence_diagram_plot.addItem(sequence_frame)
+
+
+class Frame(QObject):
+    """
+    This class corresponds to the architecture of a frame
+      of the graph for a particular variation. It recieves
+      max_end time of al the variation, because otherwise
+      as you create frames the pulses to the user might seem
+      to change even though they are fixed because of having each
+      frame a different end time.
+    """
+
+    def __init__(self, channel_tags_colors, sequences, variation, max_end):
+        super().__init__()
+        self.channel_tags_colors = channel_tags_colors  # [channel.tag,channel.label]
+        self.sequences = sequences
+        self.variation = variation
+        self.plot_sequences = []
+
+    def display_frame(self):
+        """
+        Plots sequences of pulses as stacked Heaviside (step) functions
+        into the provided pyqtgraph PlotWidget.
+
+        Parameters:
+        - graphWidget: a pg.PlotWidget instance (e.g. self.ui.graphWidget)
+        - sequences: list of lists of Pulse objects, one list per channel
+        """
+        # First, find the global end time (latest end time across all pulses)
+        global_end = 0
+        for seq in self.sequences:
+            for pulse in seq:
+                if pulse.end_tail > global_end:
+                    global_end = pulse.end_tail
+        for i, seq in enumerate(self.sequences):
+            color = self.channel_tags_colors[i][1]
+            if color == "red":
+                color = "r"
+            elif color == "green":
+                color = "g"
+            elif color == "yellow":
+                color = "yellow"
+            elif color == "orange":
+                color = "#FF5733"
+            elif color == "blue":
+                color = "blue"
+            elif color == "pink":
+                color = "pink"
+            elif color == "white":
+                color = "white"
+            elif color == "apd":
+                color = "orange"
+            elif color == "microwave":
+                color = "microwave"
+            x = []  # x-axis values (time)
+            y = []  # y-axis values (level for Heaviside + offset)
+
+            offset = 2 * i  # vertical space between channels
+
+            # Sort the pulses in this channel by start time
+            seq = sorted(seq, key=lambda p: p.start_tail)
+
+            last_end = 0  # Tracks the end of the last pulse to detect gaps
+
+            for pulse in seq:
+                # If there is a gap before the next pulse, draw a flat line at 0
+                if pulse.start_tail > last_end:
+                    x.extend([last_end, pulse.start_tail])
+                    y.extend([offset, offset])  # Flat baseline
+
+                # Rising edge: step up at start
+                x.append(pulse.start_tail)
+                y.append(offset)
+
+                x.append(pulse.start_tail)
+                y.append(offset + 1)
+
+                # High level: flat line from start to end
+                x.append(pulse.end_tail)
+                y.append(offset + 1)
+
+                # Falling edge: step down at end
+                x.append(pulse.end_tail)
+                y.append(offset)
+
+                last_end = pulse.end_tail  # Update the last end for gap checking
+            # If the last pulse ends before the global end, extend the flat line
+            if last_end < global_end:
+                x.extend([last_end, global_end])
+                y.extend([offset, offset])
+            # Optionally, draw a flat tail after the last pulse
+            # x.append(last_end + 1)
+            # y.append(offset)
+            # Ensure x has one more element than y
+            x.append(global_end)
+            # y.append(offset)
+            # Create a PlotDataItem with step mode to mimic Heaviside function
+            plot_item = pg.PlotDataItem(
+                x,
+                y,
+                stepMode=True,  # Important for square wave behavior
+                pen={"color": color, "width": 2},  # Line thickness
+            )
+
+            # Add the pulse trace to frame list
+            self.plot_sequences.append(plot_item)
+            channel_tag = str(
+                self.channel_tags_colors[i][0]
+            )  # Assuming the first element is the tag
+            text_item = pg.TextItem(
+                text=channel_tag,
+                color=color,  # Match the text color to the line color
+                anchor=(0, 1),  # Align the text to the top left
+            )
+            text_item.setPos(
+                x[0], offset + 1.25
+            )  # Position the text slightly above the sequence
+            self.plot_sequences.append(
+                text_item
+            )  # Add the TextItem to the sequence list
 
 
 if __name__ == "__main__":
